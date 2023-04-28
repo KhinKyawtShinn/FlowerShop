763fd187d9e48ddfbb4212a7b711e7d1801ab782
Naw Hkoo Ku Htoo <122962523+kooku2@users.noreply.github.com>
2023-04-28 14:34:12 +0700

    Update Tulip.java

--- Tulip.java
+++ Tulip.java
@@ -1,20 +1,26 @@
 public class Tulip implements IFlower{
+
+    @Override
     public String Color() {
         return "White";
     }
 
+    @Override
     public String Fragrance() {
         return "Honey-like";
     }
 
+    @Override
     public String Language(){
         return "perfect and deep love";
     }
 
+    @Override
     public boolean IsIndoor(){
         return false;
     }
 
+    @Override
     public double Price(){
         return 30.00;
     }
