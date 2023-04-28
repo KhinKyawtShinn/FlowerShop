2627cff20ca325b30dacc37e3a279bb10ff9ea28
Nang Pyae Pyae Phyoe Aein <122954406+Nang-Pyae-Pyae-Phyoe-Aein@users.noreply.github.com>
2023-04-28 14:08:51 +0700

    Create Camellia.java

---
+++ Camellia.java
@@ -0,0 +1,24 @@
+public abstract class Camellia implements IFlower{
+
+    @Override
+    public String Color(){
+        return "Rosy pink";
+    };
+
+    public String Fragrance(){
+        return "No smell";
+    };
+
+    public String Language(){
+        return "Symbol of love, adoration and longing";
+    };
+
+    public Boolean isIndoor(){
+        return true;
+    };
+
+    public double Price(){
+        return 20.00;
+    };
+
+}
