032c72840b115e50a6a5f31d7c4ff614272de890
nang-ying-lao-hsaing <122967319+nang-ying-lao-hsaing@users.noreply.github.com>
2023-04-28 20:45:30 +0700

    Update main flower.java

--- main flower.java
+++ main flower.java
@@ -1,18 +1,24 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.*;
 import java.lang.NumberFormatException;
 
 public class mainflower{
 
     public void start(){
-        IFlower[] flowers = new IFlower[3];
-        flowers[0] = new Lavender();
-        flowers[1] = new Tulip();
-        flowers[2] = new Camellia();
+        ArrayList<IFlower> flowers = new ArrayList<>();
+        
+        IFlower flower1 = new Lavender();
+        IFlower flower2 = new Tulip();
+        IFlower flower3 = new Camellia();
 
+        flowers.add(flower1);
+        flowers.add(flower2);
+        flowers.add(flower3); 
+        
         flowerDetails(flowers);
 
     }
-    private void flowerDetails(IFlower[] list){
+    private void flowerDetails(ArrayList<IFlower> list){
         System.out.println("--- Welcome to MARY'S BLOSSOM FLOWER SHOP ----\nFlower List:\n1. Lavender\n2. Tulip\n3. Camellia\n4. Exit\n---------------------------");
         Scanner keyboard = new Scanner(System.in);
         System.out.print("Please enter your choice : ");
@@ -20,15 +26,15 @@ public class mainflower{
         
         switch(choice){
             case 1:
-                System.out.println("You choose "+ list[0].Name()+".\nColor : "+list[0].Color()+"\nFragrance : "+list[0].Fragrance()+"\nLanguage of flower : "+list[0].Language()+"\nIndoor? : "+list[0].isIndoor()+"\nPrice : "+list[0].Price());
+                System.out.println("You chose "+ list.get(0).Name()+".\nColor : "+list.get(0).Color()+"\nFragrance : "+list.get(0).Fragrance()+"\nLanguage of flower : "+list.get(0).Language()+"\nIndoor? : "+list.get(0).isIndoor()+"\nPrice : "+list.get(0).Price());
                 Calculateprice(list, 0);
                 break;
             case 2:
-                System.out.println("You choose "+ list[1].Name()+".\nColor : "+list[1].Color()+"\nFragrance : "+list[1].Fragrance()+"\nLanguage of flower : "+list[1].Language()+"\nIndoor? : "+list[1].isIndoor()+"\nPrice : "+list[1].Price());
+                System.out.println("You chose "+ list.get(1).Name()+".\nColor : "+list.get(1).Color()+"\nFragrance : "+list.get(1).Fragrance()+"\nLanguage of flower : "+list.get(1).Language()+"\nIndoor? : "+list.get(1).isIndoor()+"\nPrice : "+list.get(1).Price());
                 Calculateprice(list, 1);
                 break;
             case 3:
-                System.out.println("You choose "+ list[2].Name()+".\nColor : "+list[2].Color()+"\nFragrance : "+list[2].Fragrance()+"\nLanguage of flower : "+list[2].Language()+"\nIndoor? : "+list[2].isIndoor()+"\nPrice : "+list[2].Price());
+                System.out.println("You chose "+ list.get(2).Name()+".\nColor : "+list.get(2).Color()+"\nFragrance : "+list.get(2).Fragrance()+"\nLanguage of flower : "+list.get(2).Language()+"\nIndoor? : "+list.get(2).isIndoor()+"\nPrice : "+list.get(2).Price());
                 Calculateprice(list, 2);
                 break;
             case 4:
@@ -41,17 +47,17 @@ public class mainflower{
       
 
     }
-    public void Calculateprice(IFlower[] list, int index_){
+    public void Calculateprice(ArrayList<IFlower> list, int index_){
         try{
             Scanner keyboard = new Scanner(System.in);
             System.out.print("How many : ");
             int amount = keyboard.nextInt();
-            double total = amount * list[index_].Price();
+            double total = amount * list.get(index_).Price();
             System.out.println("The total is "+total+"$");
         }catch(NumberFormatException e){
             System.out.println(e.getMessage());
         }
-        System.out.println("Do you want to add on more : ")
+        System.out.println("Do you want to add on more : ");
     }
     public static void main(String[] args) {
         new mainflower().start();
