c236215cc0f2da74f7f81b8dab42be199dd7a9ab
Naw Hkoo Ku Htoo <122962523+kooku2@users.noreply.github.com>
2023-04-28 14:07:05 +0700

    Create Tulip.java

---
+++ Tulip.java
@@ -0,0 +1,21 @@
+public class Tulip implements IFlower{
+    public String Color() {
+        return "White";
+    }
+
+    public String Fragrance() {
+        return "Honey-like";
+    }
+
+    public String Language(){
+        return "perfect and deep love";
+    }
+
+    public boolean IsIndoor(){
+        return false;
+    }
+
+    public double Price(){
+        return 30.00;
+    }
+}
