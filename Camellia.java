fa46837632efd539f41fb14e5e64b9c2489c95d6
Naw Hkoo Ku Htoo <122962523+kooku2@users.noreply.github.com>
2023-05-04 14:50:58 +0700

    Update Camellia.java

--- Camellia.java
+++ Camellia.java
@@ -1,27 +1,32 @@
-public abstract class Camellia implements IFlower{
+public class Camellia implements IFlower{
 
     @Override
     public String Name(){
         return "Camellia";
     }
+
+    @Override
     public String Color(){
         return "Rosy pink";
     }
 
+    @Override
     public String Fragrance(){
         return "No smell";
     }
 
+    @Override
     public String Language(){
         return "Symbol of love, adoration and longing";
     }
 
-    public Boolean isIndoor(){
+    @Override
+    public boolean isIndoor(){
         return false;
     }
 
+    @Override
     public double Price(){
         return 20.00;
     }
-
 }
